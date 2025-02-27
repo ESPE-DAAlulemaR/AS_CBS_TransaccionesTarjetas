2157f4011e6fb7127c7153fea8bfeb19e9089ed9
Dannyel <dannyel.alulema.2001@gmail.com>
2025-02-26 19:55:16 -0500

    Generacion de cuotas

--- src/main/java/com/banquito/cbs/tarjetas/servicio/DiferidoService.java
+++ src/main/java/com/banquito/cbs/tarjetas/servicio/DiferidoService.java
@@ -1,5 +1,6 @@
 package com.banquito.cbs.tarjetas.servicio;
 
+import com.banquito.cbs.tarjetas.modelo.Cuota;
 import com.banquito.cbs.tarjetas.modelo.Diferido;
 import com.banquito.cbs.tarjetas.modelo.TransaccionTarjeta;
 import com.banquito.cbs.tarjetas.repositorio.DiferidoRepository;
@@ -7,10 +8,17 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DiferidoService {
     private final DiferidoRepository repositorio;
+
+    private static final String ESTADO_COUTA_PENDIENTE = "PEN";
+    private static final String ESTADO_COUTA_PAGADA = "PAG";
+    private static final String ESTADO_COUTA_VENCIDA = "VEN";
+    private static final String ESTADO_COUTA_MORA = "MOR";
 
     public DiferidoService(DiferidoRepository repositorio) {
         this.repositorio = repositorio;
@@ -28,11 +36,43 @@ public class DiferidoService {
 
         BigDecimal tasaAnualDecimal = tasaAnual.divide(BigDecimal.valueOf(100), 6, RoundingMode.HALF_UP);
         BigDecimal tasaMensual = tasaAnualDecimal.divide(BigDecimal.valueOf(12), 6, RoundingMode.HALF_UP);
+
         BigDecimal factorCompuesto = BigDecimal.ONE.add(tasaMensual).pow(cuotas);
-        BigDecimal valorDeuda = principal.multiply(factorCompuesto).setScale(2, RoundingMode.HALF_UP);
+        BigDecimal valorDeuda = principal
+                .multiply(factorCompuesto)
+                .setScale(2, RoundingMode.HALF_UP);
 
         diferido.setMontoTotal(valorDeuda);
-        
+        List<Cuota> listaCuotas = new ArrayList<>();
+        BigDecimal cuotaFija = valorDeuda
+                .divide(BigDecimal.valueOf(cuotas), 2, RoundingMode.HALF_UP);
+        BigDecimal saldoPendiente = principal;
+
+        for (int i = 1; i <= cuotas; i++) {
+            BigDecimal interesCuota = saldoPendiente
+                    .multiply(tasaMensual)
+                    .setScale(2, RoundingMode.HALF_UP);
+
+            BigDecimal capitalCuota = cuotaFija
+                    .subtract(interesCuota)
+                    .setScale(2, RoundingMode.HALF_UP);
+
+            saldoPendiente = saldoPendiente
+                    .add(interesCuota)
+                    .subtract(cuotaFija)
+                    .setScale(2, RoundingMode.HALF_UP);
+
+            Cuota cuota = new Cuota();
+            cuota.setNumeroCuota(i);
+            cuota.setCapital(capitalCuota);
+            cuota.setInteres(interesCuota);
+            cuota.setTotal(cuotaFija);
+            cuota.setEstado(DiferidoService.ESTADO_COUTA_PENDIENTE);
+
+            listaCuotas.add(cuota);
+        }
+
+        diferido.setCuotas(listaCuotas);
         this.repositorio.save(diferido);
     }
 }
